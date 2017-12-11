3db3092bec43ba7e882d317c790f3fc82e1a9045
LXJ <15738773653@163.com>
2017-12-11 16:53:40 +0800

    添加了Customer.java文件，请注意更新

---
+++ Customer.java
@@ -0,0 +1,114 @@
+package com.itheima.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.opensymphony.xwork2.inject.Scope.Strategy;
+
+/*
+ * 
+ * `cust_id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT '客户编号(主键)',
+  `cust_name` varchar(32) NOT NULL COMMENT '客户名称(公司名称)',
+  `cust_source` varchar(32) DEFAULT NULL COMMENT '客户信息来源',
+  `cust_industry` varchar(32) DEFAULT NULL COMMENT '客户所属行业',
+  `cust_level` varchar(32) DEFAULT NULL COMMENT '客户级别',
+  `cust_address` varchar(128) DEFAULT NULL COMMENT '客户联系地址',
+  `cust_phone` varchar(64) DEFAULT NULL COMMENT '客户联系电话',
+ */
+@Entity
+@Table(name="cst_customer")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="cust_id")
+	private Long custId;
+	
+	
+	@Column(name="cust_name")
+	private String custName;
+	
+//配置多对一	
+	
+	
+	
+	@ManyToOne(targetEntity=BaseDict.class)
+	@JoinColumn(name="cust_source",referencedColumnName="dict_id")
+	private BaseDict custSource;
+	
+	@ManyToOne(targetEntity=BaseDict.class)
+	@JoinColumn(name="cust_level",referencedColumnName="dict_id")
+	
+	private BaseDict custLevel;
+	
+	
+	
+	@Column(name="cust_industry")
+	private String custIndustry;
+	
+	
+	@Column(name="cust_address")
+	private String custAddress;
+	
+	@Column(name="cust_phone")
+	private String custPhone;
+	
+	public Long getCustId() {
+		return custId;
+	}
+	public void setCustId(Long custId) {
+		this.custId = custId;
+	}
+	public String getCustName() {
+		return custName;
+	}
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	
+	public String getCustIndustry() {
+		return custIndustry;
+	}
+	public void setCustIndustry(String custIndustry) {
+		this.custIndustry = custIndustry;
+	}
+	
+	public String getCustAddress() {
+		return custAddress;
+	}
+	public void setCustAddress(String custAddress) {
+		this.custAddress = custAddress;
+	}
+	public String getCustPhone() {
+		return custPhone;
+	}
+	public void setCustPhone(String custPhone) {
+		this.custPhone = custPhone;
+	}
+	public BaseDict getCustSource() {
+		return custSource;
+	}
+	public void setCustSource(BaseDict custSource) {
+		this.custSource = custSource;
+	}
+	public BaseDict getCustLevel() {
+		return custLevel;
+	}
+	public void setCustLevel(BaseDict custLevel) {
+		this.custLevel = custLevel;
+	}
+	@Override
+	public String toString() {
+		return "Customer [custId=" + custId + ", custName=" + custName + ", custSource=" + custSource + ", custLevel="
+				+ custLevel + ", custIndustry=" + custIndustry + ", custAddress=" + custAddress + ", custPhone="
+				+ custPhone + "]";
+	}
+	
+	
+	
+}
